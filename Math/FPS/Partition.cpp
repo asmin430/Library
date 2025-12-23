da8e32121e1ac60e7d7b556ac937b9bd2086222a
asmin430 <asumiringo2007@gmail.com>
2025-12-23 11:32:39 +0900

    Add files via upload

---
+++ Math/FPS/Partition.cpp
@@ -0,0 +1,11 @@
+// depend on NTTFriendlyFormalPowerSeries.cpp
+template<typename T>
+FPS<T> partition(int N){
+    FPS<T> po(N + 1);
+    po[0] = 1;
+    for(int k = 1; k <= N; ++k){
+        if(1LL * k * (3 * k + 1) / 2 <= N) po[k * (3 * k + 1) / 2] += (k % 2 ? -1 : 1);
+        if(1LL * k * (3 * k - 1) / 2 <= N) po[k * (3 * k - 1) / 2] += (k % 2 ? -1 : 1);
+    }
+    return po.inv();
+}
